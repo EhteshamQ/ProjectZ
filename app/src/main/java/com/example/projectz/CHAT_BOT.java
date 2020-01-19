b2b25fd08af6d37add7b792f983579a9ce326aa6
Ehtesham <ehtesham_1234@live.com>
2020-01-19 16:12:41 +0530

    HAAAN

---
+++ app/src/main/java/com/example/projectz/CHAT_BOT.java
@@ -0,0 +1,14 @@
+package com.example.projectz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CHAT_BOT extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat__bot);
+    }
+}
--- app/src/main/java/com/example/projectz/MainActivity.java
+++ app/src/main/java/com/example/projectz/MainActivity.java
@@ -1,14 +1,110 @@
 package com.example.projectz;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import com.google.gson.JsonElement;
+
+import java.util.Map;
+
+import ai.api.AIListener;
+import ai.api.android.AIConfiguration;
+import ai.api.android.AIService;
+import ai.api.model.AIError;
+import ai.api.model.AIResponse;
+import ai.api.model.Result;
+
+public class MainActivity extends AppCompatActivity implements AIListener  {
+    public Button button;
+    TextView textView;
+
+    private AIService aiService;
+    boolean b=true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button=findViewById(R.id.Listen);
+        textView=findViewById(R.id.textView);
+
+
+
+        final AIConfiguration config = new AIConfiguration("cc5875b97b874e9ba10fe3330f701d9c",
+                AIConfiguration.SupportedLanguages.English,
+                AIConfiguration.RecognitionEngine.System);
+
+       aiService = AIService.getService(this, config);
+        aiService.setListener(this);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO},1);
+        }
+
+    }public void listenButtonOnClick(final View view) {
+        b=false;
+        if(!b) {
+            aiService.startListening();
+        }
+        else
+        {
+            aiService.stopListening();
+        }
     }
+
+
+    @Override
+    public void onResult(AIResponse response) {
+        Result result = response.getResult();
+
+        // Get parameters
+        String parameterString = "";
+        if (result.getParameters() != null && !result.getParameters().isEmpty()) {
+            for (final Map.Entry<String, JsonElement> entry : result.getParameters().entrySet()) {
+                parameterString += "(" + entry.getKey() + ", " + entry.getValue() + ") ";
+            }
+        }
+
+        // Show results in TextView.
+        textView.setText("Query:" + result.getResolvedQuery() +
+                "\nAction: " + result.getAction() +
+                "\nResponse "
+        + result.getFulfillment().getSpeech());
+
+    }
+
+    @Override
+    public void onError(AIError error) {
+        textView.setText(error.toString());
+
+    }
+
+    @Override
+    public void onAudioLevel(float level) {
+
+    }
+
+    @Override
+    public void onListeningStarted() {
+
+    }
+
+    @Override
+    public void onListeningCanceled() {
+
+    }
+
+    @Override
+    public void onListeningFinished() {
+
+    }
+
+
 }
